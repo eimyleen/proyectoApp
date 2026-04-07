3b591e0ee3d1263d8285157481d42267e9569155
ManuelAgNz <manuel.11282.31220.jd@gmail.com>
2026-04-07 15:09:45 -0600

    FEAT
    Instalación de Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', function () {
-    return view('login');
-});
-
 Route::get('/dashboard', function () {
-    return view('layouts.dashboard');
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/dashboard/alumno', function () {
-    return view('dashboard.alumno.alumno');
-});
-
-Route::get('/dashboard/alumno/expediente', function () {
-    return view('dashboard.alumno.alumno_expediente');
-});
-
-Route::get('/dashboard/alumno/calificaciones', function () {
-    return view('dashboard.alumno.alumno_calificaciones');
-});
-
-Route::get('/dashboard/maestro', function () {
-    return view('dashboard.maestro.maestro');
-});
-
-Route::get('/dashboard/maestro/perfil', function () {
-    return view('dashboard.maestro.perfil_maestro');
-});
-
-Route::get('/dashboard/maestro/grupos', function () {
-    return view('dashboard.maestro.grupos');
-});
-
-Route::get('/dashboard/maestro/expediente/alumno', function () {
-    return view('dashboard.maestro.expediente_alumno_maestro');
-});
-
-Route::get('/dashboard/admin/carrera', function () {
-    return view('dashboard.admin.admin_carrera');
-});
-
-Route::get('/dashboard/admin', function () {
-    return view('dashboard.admin.admin');
-});
-
-Route::get('/dashboard/admin/alumno/expediente', function () {
-    return view('dashboard.admin.admin_alumno_expediente');
-});
-
-Route::get('/dashboard/admin/maestro/perfil', function () {
-    return view('dashboard.admin.admin_maestro_perfil');
-});
-
-Route::get('/dashboard/admin/perfil', function () {
-    return view('dashboard.admin.admin_perfil');
-});
+require __DIR__.'/auth.php';
