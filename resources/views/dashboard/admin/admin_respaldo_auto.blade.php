114f43daae68ff944cff9848759711d4ae05d30b
jexus <jexus_gab@hotmail.com>
2026-04-13 23:57:13 -0700

    FEAT- inclusión de basevisual para respaldos

---
+++ resources/views/dashboard/admin/admin_respaldo_auto.blade.php
@@ -0,0 +1,14 @@
+<!-- Dashboard Admin - Módulo de gestión de carreras y usuarios -->
+@extends('layouts.dashboard')
+
+@section('title', 'Administrador - Carreras')
+
+@section('subtitle', 'Gestiona las carreras de la universidad')
+
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('css/dashboard_admin.css') }}">
+@endpush
+
+@section('content')
+    
+@endsection
